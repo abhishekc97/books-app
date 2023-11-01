0453ef8b40736a650b3c2c4410e080bc1c2c9936
Abhishek Choudhari <abhishekc97@gmail.com>
2023-11-01 22:13:54 +0530

    added a back button

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { useRouter } from 'next/navigation';
+
 import debounce from 'lodash.debounce';
 import BookCard from '@/components/BookCard';
 import { searchBookAPI } from '@/utils/booksAPI';
@@ -18,9 +21,16 @@ export default function SearchPage() {
 	const [query, setQuery] = useState('');
 	const [results, setResults] = useState<Book[]>([]);
 
+	const router = useRouter();
+
 	// handler for updating books list
 	function handleBookChangeSuccess() {
 		console.log('Blank');
+	}
+
+	// navigate to /
+	function handleBackClick() {
+		router.push('/');
 	}
 
 	useEffect(() => {
@@ -51,15 +61,24 @@ export default function SearchPage() {
 	}, [query]);
 
 	return (
-		<div className="searchContainer min-h-screen flex flex-col items-center justify-center">
-			<div className="searchInput mb-12">
-				<input
-					type="text"
-					className="border px-8"
-					placeholder="Search for a book"
-					value={query}
-					onChange={(e) => setQuery(e.target.value)}
-				/>
+		<div className="searchContainer min-h-screen flex flex-col items-center">
+			<div className="topSection flex items-center justify-center w-full px-16 sm:my-6 md:my-6 mb-8 h-16 min-h-16 relative">
+				<Button
+					variant="outline-secondary"
+					onClick={handleBackClick}
+					className="absolute left-4 w-fit border rounded-md px-4"
+				>
+					Back
+				</Button>
+				<div className="searchInput">
+					<input
+						type="text"
+						className="border px-8"
+						placeholder="Search for a book"
+						value={query}
+						onChange={(e) => setQuery(e.target.value)}
+					/>
+				</div>
 			</div>
 			<div className="searchResultsContainer grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:h-[632px] mx-auto mb-8">
 				{results.map((book) => (
